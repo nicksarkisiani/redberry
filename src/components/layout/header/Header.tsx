f6e21c897de26dc2e3aded0109a0117818348a9e
Haster1337 <hasterpro@gmail.com>
2024-09-17 01:11:35 +0400

    basic folder structure and header

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <img src={require('../../../assets/images/logo.png')} alt="logo"/>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
