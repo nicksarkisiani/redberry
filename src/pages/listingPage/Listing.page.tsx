cb043aa2feac5cd53494a8aa3e65238ed884f781
Haster1337 <hasterpro@gmail.com>
2024-09-21 23:02:50 +0400

    static agent?

--- src/pages/listingPage/Listing.page.tsx
+++ src/pages/listingPage/Listing.page.tsx
@@ -5,7 +5,7 @@ import {IEstate} from "../../types/store";
 import {$api} from "../../http";
 import {AxiosResponse} from "axios";
 import {Button, Typography} from "antd";
-import {ArrowLeftOutlined} from "@ant-design/icons";
+import {ArrowLeftOutlined, MailOutlined, PhoneOutlined} from "@ant-design/icons";
 import styles from "./listingPage.module.scss"
 import Image from "../../components/shared/image/Image";
 import Field from "../../components/shared/field/Field";
@@ -41,7 +41,8 @@ const ListingPage = () => {
 
                                 <div className={styles.locationInfo}>
                                     <Field fontSize={24} color={"#808A93"}><LocationSvg/> {listing.address}</Field>
-                                    <Field fontSize={24} color={"#808A93"}><AreaSvg/> {listing.area}</Field>
+                                    <Field fontSize={24} color={"#808A93"}><AreaSvg/> {listing.area} მ <sup
+                                        style={{fontSize: "11px"}}>2</sup></Field>
                                     <Field fontSize={24} color={"#808A93"}><BedSvg/> საძინებელი {listing.bedrooms}
                                     </Field>
                                     <Field fontSize={24} color={"#808A93"}><SignSvg/> საფოსტო ინდექსი {listing.zip_code}
@@ -50,17 +51,46 @@ const ListingPage = () => {
                             </div>
 
                             <div className={styles.description}>
-                                <Field lineHeight={"26px"} fontSize={16} color={"#808A93"}>იყიდება ბინა ჭავჭავაძის ქუჩაზე, ვაკეში. ბინა არის
+                                <Field lineHeight={"26px"} fontSize={16} color={"#808A93"}>იყიდება ბინა ჭავჭავაძის
+                                    ქუჩაზე, ვაკეში. ბინა არის
                                     ახალი რემონტით, ორი საძინებლითა და დიდი აივნებით. მოწყობილია ავეჯითა და
                                     ტექნიკით. </Field>
                             </div>
                         </div>
 
                         <div className={styles.agentInfo}>
+                            <div className={styles.agent}>
+                                <div className={styles.agentAbout}>
+                                    <div className={styles.thumbnail}>
+                                        <Image width={"72px"} height={"72px"} src={require("../../assets/images/woman.png")}  />
+                                    </div>
+                                    <div className={styles.agentName}>
+                                        <Typography.Title level={4}>სოფიო გელოვანი</Typography.Title>
+                                        <Field fontSize={14} color={"#676E76"}>აგენტი</Field>
+                                    </div>
+                                </div>
 
+                                <div className={styles.agentContact}>
+                                    <Field fontSize={14} color={"#808A93"}><MailOutlined /> sophio.gelovani@redberry.ge</Field>
+                                    <Field fontSize={14} color={"#808A93"}><PhoneOutlined /> 577 777 777</Field>
+                                </div>
+                            </div>
+
+                            <div className={styles.delete}>
+                                <Button className={styles.button}>
+                                    ლისტინგის წაშლა
+                                </Button>
+                            </div>
                         </div>
                     </div>
 
+                </div>
+
+                <div>
+                    <Typography.Title level={2}>ბინები მსგავს ლოკაციაზე</Typography.Title>
+                    <div>
+
+                    </div>
                 </div>
             </div>
             <div>
